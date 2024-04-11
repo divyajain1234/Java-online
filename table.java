b2ae86a034a04cdd703c64f3b1ae043d014a4eb0
divyajain1234 <143693572+divyajain1234@users.noreply.github.com>
2024-04-11 19:05:28 +0530

    table.java

---
+++ table.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class main{
+    public static void main(String args[]){
+        System.out.println("Enter the value of n");
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(n*i);
+        }
+           
+    }
+}
