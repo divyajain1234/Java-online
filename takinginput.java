ec5eda016a79b4e829058838c0a5f5e119136243
divyajain1234 <143693572+divyajain1234@users.noreply.github.com>
2024-04-11 17:20:33 +0530

    takinginput.java

---
+++ takinginput.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class main{
+    public static void main(String args[]){
+        System.out.println("Enter your name");
+        Scanner sc = new Scanner(System.in);
+        String name= sc.nextLine();
+        System.out.print("your name is ");
+        System.out.println(name);
+    }
+}
