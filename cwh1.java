41a51a9d5d1b5df3554cf344d63603432afe01dc
divyajain1234 <143693572+divyajain1234@users.noreply.github.com>
2024-04-13 21:29:24 +0530

    cwh1.java

---
+++ cwh1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Divya{
+public static void main(String [] args){
+    Scanner sc = new Scanner(System.in);
+   /* System.out.println("Enter the value of a");
+    int a= sc.nextInt();
+    System.out.println("Enter the value of b");
+    int b = sc.nextInt();
+    int sum = a+b;
+    System.out.println(sum);*/
+    boolean b1=sc.hasNextInt();
+    System.out.println(b1);
+} 
+    }
