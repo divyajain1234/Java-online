c07aff4472a763cd953c45d3d7b3138e86219b63
divyajain1234 <143693572+divyajain1234@users.noreply.github.com>
2024-04-13 21:57:23 +0530

    divybooks.java

---
+++ divybooks.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Divya{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any word");
+        String name= sc.nextLine();
+        System.out.println("All "+name+"  are available");
+        System.out.println("All books and notebooks are available in minimum price");
+    }
+}
