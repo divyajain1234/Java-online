1dfd6fac9bd310cb387aa45be13bc76dbd80a513
divyajain1234 <143693572+divyajain1234@users.noreply.github.com>
2024-04-13 22:02:03 +0530

    boolean.java

---
+++ boolean.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Divya{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any number");
+        boolean b1= sc.hasNextInt();
+       System.out.println(b1);
+    }
+}
