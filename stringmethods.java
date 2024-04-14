d9cef19c98348dcec8e4452b53a6830a02d93f70
divyajain1234 <143693572+divyajain1234@users.noreply.github.com>
2024-04-14 11:50:57 +0530

    stringmethods.java

---
+++ stringmethods.java
@@ -0,0 +1,30 @@
+public class Main{
+    public static void main(String[] args){
+       // String name = "Divya";
+       // int value = name.length();
+        //System.out.println(value);
+        
+        // String value = name.toLowerCase();
+        // System.out.println(value);
+        
+        //String value = name.toUpperCase();
+        //System.out.println(value);
+        
+        // String name = "     Divya    ";
+        //  String value = name.trim();
+        // System.out.println(value);
+        
+        
+        // String name = "Divya";
+        // String value = name.substring(2);
+        // System.out.println(value);
+        
+        // String name = "Divya";
+        // String value = name.substring(2,4);
+        //  System.out.println(value);
+    
+        // String name = "Divya";
+        // String value = name.replace('y','i');
+        //  System.out.println(value);
+    }
+}
