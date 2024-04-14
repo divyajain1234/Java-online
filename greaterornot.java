e8e9dc5f581fbec12e72d12dca98b0e3e4e9bebf
divyajain1234 <143693572+divyajain1234@users.noreply.github.com>
2024-04-14 11:13:46 +0530

    greaterornot.java

---
+++ greaterornot.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String [] args){
+        System.out.println("Enter any number");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+       
+        System.out.println(n>12 );
+        
+    }
+}
